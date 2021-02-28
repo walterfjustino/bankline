efeea82c38269ae61d154a1498bfd26e9b4fe12e
lucasribeiro <lucasribeiro@ryzen>
2021-02-28 14:24:35 -0300

    Adicionando os códigos das exceptions

--- src/main/java/com/bankline/bankline/data/exception/PlanoContaNotFoundException.java
+++ src/main/java/com/bankline/bankline/data/exception/PlanoContaNotFoundException.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PlanoContaNotFoundException extends Exception {
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public PlanoContaNotFoundException(Long idPlanoConta) {
 		super(String.format("Plano de Conta não encontrado: ", idPlanoConta));
 		
--- src/main/java/com/bankline/bankline/data/exception/SaldoInsulficienteException.java
+++ src/main/java/com/bankline/bankline/data/exception/SaldoInsulficienteException.java
@@ -1,5 +1,9 @@
 package com.bankline.bankline.data.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class SaldoInsulficienteException extends Exception {
 
 	private static final long serialVersionUID = 1L;
