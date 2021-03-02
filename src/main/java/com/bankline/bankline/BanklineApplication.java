de8d06d33c1a074210654157c5c89229b20007e5
Samuel <squoliver@gmail.com>
2021-03-02 18:55:55 -0300

    Add o docket na aplicação

--- src/main/java/com/bankline/bankline/BanklineApplication.java
+++ src/main/java/com/bankline/bankline/BanklineApplication.java
@@ -1,7 +1,16 @@
 package com.bankline.bankline;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,5 +20,26 @@ public class BanklineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApplication.class, args);
 	}
-
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo.swagger"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"API Bankline",
+				"Gama Academy - Projeto final: API - Bankline",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Burndown Team", "https://xpcorp.gama.academy/", "teste@gmail.com"),
+				"API License",
+				"https://xpcorp.gama.academy/",
+				Collections.emptyList());
+	}
 }
