6570d62b6bb859576fc24087ae0a3cd0cc667e18
Raífe <raife.tec@gmail.com>
2021-03-03 22:02:53 -0300

    Criação do endPoint contasController

--- src/main/java/com/bankline/bankline/data/controller/ContasController.java
+++ src/main/java/com/bankline/bankline/data/controller/ContasController.java
@@ -1,19 +1,49 @@
 package com.bankline.bankline.data.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bankline.bankline.data.service.ContaService;
+import com.bankline.bankline.data.dto.PlanoContaDTO;
+import com.bankline.bankline.data.exception.UsuarioNaoEncontradoException;
+import com.bankline.bankline.data.model.PlanoConta;
+import com.bankline.bankline.data.model.Usuario;
+import com.bankline.bankline.data.repository.PlanoContaRepository;
+import com.bankline.bankline.data.service.PlanoContaService;
 
 @RestController
 @RequestMapping(value = "api/contas")
 public class ContasController {
 	
 	@Autowired
-	private ContaService contaService;
+	private PlanoContaService planoContaService;
 	
+	@Autowired
+	private PlanoContaRepository planoContaRepository;
 	
+	@PostMapping
+	public ResponseEntity<?> novoPlanoConta(PlanoContaDTO planoConta) {
+		try {	
+			planoConta = planoContaService.salvarPlanoConta(planoConta);
+			return ResponseEntity.status(HttpStatus.CREATED).body(planoConta);
+			
+		} catch (UsuarioNaoEncontradoException log) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(log.getMessage());
+		}
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<PlanoConta>> listarPlanosConta(Usuario usuario){
+		List<PlanoConta> listaPlanos = planoContaRepository.findPlanoContaDisponiveis(usuario);
+		return ResponseEntity.status(HttpStatus.OK).body(listaPlanos);
+	}
 	
 
 }
