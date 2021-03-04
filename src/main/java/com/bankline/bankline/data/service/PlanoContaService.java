f0fb39230c400a11a64a2b4ef46d264dece79cec
lucasribeiro <lucasribeiro@ryzen>
2021-03-03 22:52:01 -0300

    Ajustes

--- src/main/java/com/bankline/bankline/data/service/PlanoContaService.java
+++ src/main/java/com/bankline/bankline/data/service/PlanoContaService.java
@@ -21,33 +21,6 @@ public class PlanoContaService {
 	@Autowired
 	private UsuarioService usuarioService;
 	
-//	private final PlanoContaMapper planoContaMapper = PlanoContaMapper.INSTANCE;
-	
-	
-//	public PlanoContaService(PlanoContaRepository planoContaRepository, TransacaoRepository transacaoRepository) {
-//		this.planoContaRepository = planoContaRepository;
-//		this.transacaoRepository = transacaoRepository;
-//	}
-	
-	//Metodo Post - cadastra Plano Conta
-	
-//	public MessageResponseDTO cadastroPlanoConta(PlanoContaDTO planoContaDTO) {
-//		
-//		PlanoConta planoContaToSalvar = planoContaMapper.toModel(planoContaDTO);
-//		
-//		PlanoConta planoContaSalva = planoContaRepository.save(planoContaToSalvar);
-//		
-//		return MessageResponseDTO.builder().message("Plano conta cadastrado ID: " + planoContaSalva.getIdPlanoConta()).build();
-//		
-//	} 
-	
-	//Metodo Get - pesquisa Plano Conta por id
-	
-//	public PlanoContaDTO pesquisaPlanoContaById(Long idPlanoConta) throws PlanoContaNotFoundException {
-//		PlanoConta planoConta = planoContaRepository.findById(idPlanoConta)
-//				.orElseThrow(() -> new PlanoContaNotFoundException(idPlanoConta));
-//		return planoContaMapper.toDTO(planoConta);
-//	}
 	
 	public List<PlanoContaDTO> listaPlanoContaDisponiveis(String login) throws UsuarioNaoEncontradoException {
 		
